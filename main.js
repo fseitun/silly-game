e2d3ad3dcf25377f64b97a146e710e2aea293bbf
Fico <fseitun@gmail.com>
2021-03-30 14:08:21 -0300

    3/7

--- main.js
+++ main.js
@@ -4,3 +4,22 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor (arr) {
+        this.field = arr;
+    }
+    print() {
+        for (const i of this.field) {
+            console.log(i.join(''));   
+        } 
+    }
+}
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+myField.print();
